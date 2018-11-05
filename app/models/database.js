c0bdb2ace4b80dc67bd9bf5c72f0ecb34991b2a9
Kevin Glover-Netherton <kglovern@mail.uoguelph.ca>
2018-11-05 15:48:31 -0500

    Added passport authentication libraries

--- app/models/database.js
+++ app/models/database.js
@@ -1,5 +1,7 @@
 const path = require('path');
+// Database Configuration
 const dbConfig = require(path.join(configDir, './dbConfig'));
+// KNEX object with our configuration options
 const db = require('knex')(dbConfig);
-
+// For import
 module.exports = db;
