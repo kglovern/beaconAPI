6e0395b8c1f14cb90eec8714a4c43de0cc5897c4
MRead19 <mitchell.read@bell.net>
2018-11-15 16:58:41 -0500

    Test commit

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -3,6 +3,7 @@ const db = require(path.join(__model, 'database'));
 
 module.exports = {
   authenticate: (req, res) => {
+  	//Test branch merge
     res.send({OK: 'Login page was requested and sent!'});
   },
 };
