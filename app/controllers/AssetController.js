c0bd6d40208aaed7fd51c2b756b99a7f5bd21f61
MRead19 <mitchell.read@bell.net>
2018-11-21 10:16:28 -0500

    started assets

--- app/controllers/AssetController.js
+++ app/controllers/AssetController.js
@@ -1,2 +1,32 @@
 const path = require('path');
-const db = require(path.join(__model, 'database'));
+const jwt = require('jsonwebtoken');
+const verifyMyToken = require('../routes/verifyMyToken');
+
+module.exports = {
+	createAsset: async (req, res) => {
+    	project_id = req.query.project_id
+    	owner_id = req.query.owner_id
+    	//Need to finish
+	},
+	getAssetInfo: async (req, res) => {
+    	assetId = req.query.id
+
+    	//look up asset in database
+    	assets = await db.from('Asset').select().where({
+      		assetId: id
+    	}).first();
+
+    	//asset not found
+    	if (assets.length == 0) {
+      		res.json({
+        		message: 'this asset does not exist'
+      		});
+      	}
+
+    	res.json({
+      		assets
+      	});
+    },
+
+  },
+};
